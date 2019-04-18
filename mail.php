34ad9fda92d4c011484bf863e2deddea3bd03ff8
Derya O. Kurin <dozdemir663@gmail.com>
2019-04-18 13:07:30 -0400

    PHP mailer does not work

--- mail.php
+++ mail.php
@@ -1,56 +1,75 @@
 <?php
-use vendor/phpmailer/phpmailer/src/Exception;
-use vendor/phpmailer/phpmailer/src/PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+require '/vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+$name = test_input($_POST["name"]);
+$email = test_input($_POST["email"]);
+$phone = test_input($_POST["phone"]);
+$subject = test_input($_POST["subject"]);
+$message = test_input($_POST["message"]);
 
 
-  require '/vendor/autoload.php';
+$mySmtpHost = 'smtp.gmail.com';
+$myEmail = 'dozdemir663@gmail.com';
+$myemailPassword = 'DeryaOzdemir2017';
 
-  $mail = new PHPMailer;
+
+$Body = "<html>
+<body>
+   <h2>You have a message from www.fatmaardabicaz.av.tr website user</h2>
+   <hr>
+   <p>Isim:<br>`.$name.`</p>
+   <p>E-mail:<br>`.$email.`</p>
+   <p>Telefon: <br>`.$phone.`</p>
+   <p>Konu: <br>`.$subject.`</p>
+   <p>Mesaj:<br>`.$message.`</p>
+</body>
+</html>";
 
 try {
+
   $mail->isSMTP();
   $mail->SMTPDebug  = 2;
-  $mail->Host       = 'smtp.gmail.com';
+  $mail->Host       = $mySmtpHost;
   $mail->SMTPAuth   = true;
-  $mail->Username   = 'mymailaddress.com';
-  $mail->Password   = 'secret';
+  $mail->Username   = $myEmail;
+  $mail->Password   = $myemailPassword;
   $mail->SMTPSecure = 'tls';
   $mail->Port       = 587;
 
-  $headers = "From: ".$name." <".$email.">\r\n";
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers = "Content-type: text/html; charset-utf-8";
+  $mail->setFrom($myEmail);
+  $mail->addReplyTo($myEmail);
+  $mail->addAddress($email, $name);
+  $mail->Subject = 'Here is the subject';
 
+$mail->isHTML(true); 
+$mail->Subject = 'Here is the subject';
+$mail->Body = $message;
 
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $phone = $_POST['phone'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
-  $to = "mymailaddress.com";
-  $mail->Body    = '<html>
-               <body>
-                   <h2>You have a message from www.fatmaardabicaz.av.tr website user</h2>
-                   <hr>
-                   <p>Isim:<br>`.$name.`</p>
-                   <p>E-mail:<br>`.$email.`</p>
-                   <p>Telefon:<br>`.$phone.`</p>
-                   <p>Mesaj:<br>`.$message.`</p>
-               </body>
-           </html>';
-  $txt = " You have a message from: ".$name.".\n\n".$message;
-
-
-  $mail->setFrom($email);
-  $mail->addReplyTo('mymailaddress.com');
-  $mail->msgHTML(file_get_contents($body), __DIR__);
-  $mail->AltBody = $txt;
-
-  $mail->setLanguage('tr', 'vendor/phpmailer/phpmailer/language/phpmailer.lang-tr.php');
-  $mail->send();
-  echo 'Message has been sent';
+$mail->setLanguage('tr', 'vendor/phpmailer/phpmailer/language/phpmailer.lang-tr.php');
+    $mail->send();
+    echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function send() {
+        if (!$this->mail->send()) {
+                echo $this->mail->ErrorInfo;
+        }
+}
+
 
 ?>
